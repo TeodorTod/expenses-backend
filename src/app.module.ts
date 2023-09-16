5e6937ca32f18f59f1cd35826749766484172f85
Teodor Todorov <teodor.at.tod@gmail.com>
2023-09-16 23:36:24 +0300

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { AuthorizationModule } from './authorization/authorization.module';
 import { ExpensesModule } from './expenses/expenses.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     AuthorizationModule, 
     ExpensesModule,
-    MongooseModule.forRoot('mongodb+srv://teodor:teodor1991@cluster0.4pqun8m.mongodb.net/?retryWrites=true&w=majority')
+    MongooseModule.forRoot(`mongodb+srv://teodor:teodor1991@cluster0.4pqun8m.mongodb.net/?retryWrites=true&w=majority`)
   ],
   controllers: [AppController],
   providers: [AppService],
